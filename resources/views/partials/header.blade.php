5ca591d13afd121ac0bc6c613b894d8f2c6bf3a0
davidemontagna <davidemontagna92@gmail.com>
2022-03-03 16:02:53 +0100

    header completato

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,54 @@
 <header>
-    <div class="container">
+    <div class="additional-sites">
+        <div>
+            <a href="#">DC power&#8480; VISA&#174;</a>
+            <a href="#">additional sites <i class="fa-solid fa-caret-down"></i></a>
+        </div>        
+    </div>
+    <div class="nav_container">
       <div class="logo-dc">
-        <img src="{{asset('images/dc-logo.png')}}" alt="">
+        <img src="{{asset('images/dc-logo.png')}}" alt="dc-logo">
       </div>
       <nav>
-        <ul>
-          <li v-for="(link, index) in links" :key="index" :class="{'active' : link.current}">
-            <a href="#" >{{link.text}}</a>
+        <ul class="nav-bar">
+          <li>
+            <a href="#" >Characters</a>
+          </li>
+          <li>
+            <a href="#" >Comics</a>
+          </li>
+          <li>
+            <a href="#" >Movies</a>
+          </li>
+          <li>
+            <a href="#" >TV</a>
+          </li>
+          <li>
+            <a href="#" >Games</a>
+          </li>
+          <li>
+            <a href="#" >Collectibles</a>
+          </li>
+          <li>
+            <a href="#" >Videos</a>
+          </li>
+          <li>
+            <a href="#" >Fans</a>
+          </li>
+          <li>
+            <a href="#" >News</a>
+          </li>
+          <li>
+            <a href="#" >Shop</a>
           </li>
         </ul>
       </nav>
+      <div class="search-bar">
+        <input type="text" placeholder="Search"><label for=""><i class="fa-solid fa-magnifying-glass"></i></label>
+      </div>
+    </div>
+
+    <div class="jumbotron">
+        <img src="{{asset('images/jumbotron.jpg')}}" alt="jumbotron">
     </div>
 </header>
