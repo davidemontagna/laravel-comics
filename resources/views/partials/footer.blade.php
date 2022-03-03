4789686b4336c4bd63fec04c45ec07b22177ef2f
davidemontagna <davidemontagna92@gmail.com>
2022-03-03 17:30:17 +0100

    footer completato (esercizio completo)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -23,4 +23,78 @@
             </div>
         </div>
     </div>
+
+    <div class="foot-mid">
+        <div class="foot-links">
+            <div class="left-links">
+
+                <div class="dc-comics">
+                    DC Comics
+                    <a href="#">Characters</a>
+                    <a href="#">Comics</a>
+                    <a href="#">Movies</a>
+                    <a href="#">TV</a>
+                    <a href="#">Games</a>
+                    <a href="#">Videos</a>
+                    <a href="#">News</a>
+                </div>
+                <div class="dc-shop">
+                    Shop
+                    <a href="#">Shop DC</a>
+                    <a href="#">Shop DC Collectibles</a>
+                </div>
+            </div>
+
+            <div class="mid-links">
+                <div class="dc">
+                    Dc
+                    <a href="#">Terms of Use</a>
+                    <a href="#">Privacy policy (New)</a>
+                    <a href="#">Ad Choices</a>
+                    <a href="#">Advertising</a>
+                    <a href="#">Jobs</a>
+                    <a href="#">Subscriptions</a>
+                    <a href="#">Talent Workshops</a>
+                    <a href="#">CPSC Certificates</a>
+                    <a href="#">Ratings</a>
+                    <a href="#">Shop Help</a>
+                    <a href="#">Contact Us</a>
+                </div>            
+            </div>
+
+            <div class="right-links">
+                <div class="sites">
+                    Sites
+                    <a href="#">DC</a>
+                    <a href="#">MAD Magazine</a>
+                    <a href="#">DC Kids</a>
+                    <a href="#">DC Universe</a>
+                </div>                  
+            </div>
+            
+            
+        </div>
+       
+        <div>
+            <img src="{{asset('images/dc-logo-bg.png')}}" alt="">
+        </div>     
+    </div>      
+    <div class="rt-reserved">
+        <p>All Site Content TM and <i class="fa-regular fa-copyright"></i> 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <a href="#">Cookies Settings</a></p>
+    </div> 
+</div>
+
+<div class="foot-bottom">
+    <div class="sign-up">
+        sign-up now!
+    </div>
+    <div class="socials">
+        FOLLOW US
+        <img src="{{asset('images/footer-facebook.png')}}" alt="">
+        <img src="{{asset('images/footer-twitter.png')}}" alt="">
+        <img src="{{asset('images/footer-youtube.png')}}" alt="">
+        <img src="{{asset('images/footer-pinterest.png')}}" alt="">
+        <img src="{{asset('images/footer-periscope.png')}}" alt="">
+    </div>
+</div>
 </footer>
