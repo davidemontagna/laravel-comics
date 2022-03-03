a8acddb28a2846195f5163636ece5bf074c5dbf5
davidemontagna <davidemontagna92@gmail.com>
2022-03-03 16:56:22 +0100

    parte merchandise footer fatta

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="container-merch">
+        <div class="merch">
+            <div>
+                <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="">
+                <a href="#">Digital comics</a>
+            </div>
+            <div>
+                <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="">
+                <a href="#">DC merchandise</a>
+            </div>
+            <div>
+                <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="">
+                <a href="#">Subscription</a>
+            </div>
+            <div>
+                <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="">
+                <a href="#">Comic shop locator</a>
+            </div>
+            <div>
+                <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="">
+                <a href="#">DC power visa</a>
+            </div>
+        </div>
+    </div>
+</footer>
